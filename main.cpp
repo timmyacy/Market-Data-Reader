174c842a4f8ced7b598fea4ca30c698598ed19d8
timmyacy <timmy_ajibode@hotmail.com>
2026-05-04 22:16:56 +0100

    Add fucntionality for calculating the max drawdown and annualised vol and etc

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 
+#include <cmath>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -7,6 +8,12 @@
 #include <vector>
 using namespace std;
 namespace fs = std::filesystem;
+
+struct Statistics {
+  double mean_daily_return{};
+  double annualised_vol{};
+  double max_drawdown{};
+};
 struct CSVModel {
   string date{};
   string open{};
@@ -24,8 +31,9 @@ vector<string> scanDirectory() {
   }
   return files;
 }
-void sortData(vector<CSVModel> &model) {
-  cout << model.front().date << "\n" << model.back().date;
+void sortDate(vector<CSVModel> &model) {
+  cout << " The dates are from " << model.back().date << " to "
+       << model.front().date;
 }
 int selectCSV(const vector<string> &files) {
   int selected = 0;
@@ -55,6 +63,56 @@ int selectCSV(const vector<string> &files) {
   }
   return selected;
 }
+
+void calculate_stats(const vector<CSVModel> &data, int rowsProcessed,
+                     int rowsSkipped) {
+  Statistics stats{};
+  vector<double> daily_returns;
+
+  for (int i = 1; i < (int)data.size(); i++) {
+    if (!data[i].open.empty() && !data[i - 1].close.empty()) {
+      double today_price = stod(data[i].open);
+      double yesterday_price = stod(data[i - 1].close);
+      daily_returns.push_back((today_price - yesterday_price) /
+                              yesterday_price);
+    }
+  }
+
+  int n = (int)daily_returns.size();
+
+  double sum{};
+  for (double r : daily_returns) {
+    sum += r;
+  }
+  stats.mean_daily_return = sum / n;
+
+  double variance{};
+  for (double r : daily_returns) {
+    variance += pow(r - stats.mean_daily_return, 2);
+  }
+  stats.annualised_vol = sqrt(variance / (n - 1)) * sqrt(252);
+
+  double peak = stod(data[0].close);
+  string trough_date{};
+
+  for (int i = 1; i < (int)data.size(); i++) {
+    if (!data[i].close.empty()) {
+      double close = stod(data[i].close);
+      if (close > peak)
+        peak = close;
+      double dd = (peak - close) / peak;
+      if (dd > stats.max_drawdown) {
+        stats.max_drawdown = dd;
+        trough_date = data[i].date; // date of the worst point so far
+      }
+    }
+  }
+
+  cout << "\nMean daily return : " << stats.mean_daily_return * 100 << "%"
+       << "\nAnnualised vol    : " << stats.annualised_vol * 100 << "%";
+  cout << "\nMax drawdown      : " << stats.max_drawdown * 100 << "%"
+       << "  (trough: " << trough_date << ")\n";
+}
 void parseFile(const string fileName, vector<CSVModel> &data) {
   ifstream file(fileName);
   int rowsProcessed{0};
@@ -80,18 +138,19 @@ void parseFile(const string fileName, vector<CSVModel> &data) {
       getline(ss, token, ',');
       model.close = token;
       getline(ss, token, ',');
-      model.volume = token;
-      getline(ss, token, ',');
-      model.open = token;
-      getline(ss, token, ',');
       model.high = token;
       getline(ss, token, ',');
       model.low = token;
+      getline(ss, token, ',');
+      model.open = token;
+      getline(ss, token, ',');
+      model.volume = token;
       rowsProcessed++;
       data.push_back(model);
     }
   }
-  sortData(data);
+  sortDate(data);
+  calculate_stats(data, rowsProcessed, rowsSkipped);
 }
 
 int main() {
